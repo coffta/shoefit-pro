8e8186e4138e66a73aff2f3283af524a8095b04d
coffta <poppyflowers.4012@gmail.com>
2023-08-01 17:01:53 +0900

    トップとユーザービュー実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,21 @@
 class CustomersController < ApplicationController
+  before_action :check_user_signed_in, only: :index
+
   def index
+    # ログインしている場合の処理はビューに委ねるため、ここでは何もしない
+  end
+
+  private
+
+  def check_user_signed_in
+    if user_signed_in?
+      # ログインしている場合の処理
+      # 例えば、ログイン後の表示をするビューを表示する
+      render :index_logged_in
+    else
+      # ログインしていない場合の処理
+      # 通常のindexページを表示する
+      render :index
+    end
   end
 end
