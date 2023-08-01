8b3b80b286308a0e6d4c2747bee5547977152391
coffta <poppyflowers.4012@gmail.com>
2023-08-01 14:08:16 +0900

    新規登録・ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :customers
+
+  with_options presence: true do
+    validates :nickname
+    validates :name
+    validates :address
+    validates :phone_number
+  end
 end
